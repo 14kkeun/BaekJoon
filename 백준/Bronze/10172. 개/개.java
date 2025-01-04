c46384fcfe6eee0c97dcaea5e07f832fb29ba6aa
14kkeun <14.kkeun@gmail.com>
2025-01-04 16:59:34 +0900

    [Bronze V] Title: 개, Time: 100 ms, Memory: 14052 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("|\\_/|" + "\n");
+        bw.write("|q p|   /}" + "\n");
+        bw.write("( 0 )\"\"\"\\" + "\n");
+        bw.write("|\"^\"`    |" + "\n");
+        bw.write("||_/=\\\\__|");
+        bw.close();
+    }
+}
