7fdb70d396dabc7325a3bc76d6077a4285f316c2
14kkeun <14.kkeun@gmail.com>
2025-01-16 16:53:22 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 108 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+
+        while(n >= 5){
+            cnt += n / 5;
+            n /= 5;
+        }
+
+        bw.write(String.valueOf(cnt));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
